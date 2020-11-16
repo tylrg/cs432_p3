9c24e28c41405a1588171c59700d0e4e12d30b02
Tyler Gabriel <31976417+tylrg@users.noreply.github.com>
2020-11-16 16:38:16 -0500

    Update cli.py

--- cli.py
+++ cli.py
@@ -4,12 +4,14 @@ def main():
 main()
 
 # OPTIONS #
-# 1 sign in
-# 2 view user data (authenticated?)
-# 2a view tweets
-# 2b view sentiment score
-# 2c view last updated
-# 3. compare to other users
-# 4 find similar scores
-# 5 remove data
-# 6 sign out
+# 1 sign in (username, password)
+# 2 view user data (authenticated?)(just make sure signed in)
+# 2a view tweets (no arg)
+# 2b view sentiment score (no arg)
+# 2c view last updated (no arg)
+# 3. compare to other users (username,other_username)
+# 4 find similar scores (no arg)
+# 5 remove data (no arg)
+# 6 refresh data
+# 7 sign out
+# 0 exit
