266baa0f72382bfc10f6c06db1bcbe93ba493ad3
Tyler Gabriel <tygabriel7@gmail.com>
2020-11-17 23:44:22 -0500

    Adding some ability to get new values

--- twython_mongo.py
+++ twython_mongo.py
@@ -4,21 +4,28 @@ import pprint
 import sys
 import ssl
 
-APP_KEY = "here"
-APP_SECRET = "here"
-OAUTH_TOKEN = "here"
-OAUTH_TOKEN_SECRET = "here"
+APP_KEY = "UHz2Yr3qedZeNZxblgzqVbER6"
+APP_SECRET = "KavH3dnHPOPybPWBWXAWD7jAqfJRxDLqEK24N1m64O2fax0vM0"
+twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+ACCESS_TOKEN = twitter.obtain_access_token()
+twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 
-client = pymongo.MongoClient("mongodb+srv://tgabrie2:cs432@cs432-p3.79oxz.mongodb.net/cs432?retryWrites=true&w=majority",ssl_cert_reqs=ssl.CERT_NONE)
-db = client.cs432
-collection = db['test']
-cur = collection.find()
-for doc in cur:
-  #print(doc)
-  print(doc['test_value'])
+# client = pymongo.MongoClient("mongodb+srv://tgabrie2:cs432@cs432-p3.79oxz.mongodb.net/cs432?retryWrites=true&w=majority",ssl_cert_reqs=ssl.CERT_NONE)
+# db = client.cs432
+# collection = db['test']
+# cur = collection.find()
+# for doc in cur:
+#   #print(doc)
+#   print(doc['test_value'])
 
-# print("twython")
-# twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+print("\n\n\n\n")
+sn = input('Enter Screen Name: ')
 
-# retrieved_user_tweets = twitter.get_user_timeline(user_id="kanyewest")
+try:
+    user_timeline = twitter.get_user_timeline(screen_name= sn, count=30)
+except TwythonError as e:
+   print(e)
+for tweets in user_timeline:
+   print (tweets['text'])
 
+#print(s)
