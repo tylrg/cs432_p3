7342604f2851d3b5de57e399196b5903cdcc6337
Tyler Gabriel <tygabriel7@gmail.com>
2020-11-19 23:04:05 -0500

    cleanedup

--- vader_test.py
+++
@@ -1,9 +0,0 @@
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-
-def main():
-	analyzer = SentimentIntensityAnalyzer()
-	sentiment_dict = analyzer.polarity_scores("that is sooo lame")
-	print(sentiment_dict)
-
-main()
